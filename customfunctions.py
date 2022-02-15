903ff268658cbf1850dc3eb0e754a4f551eb688a
Haritha Weerathunga <39055744+HarithaWeerathunga@users.noreply.github.com>
2022-02-15 08:29:14 +0530

    Custom Functions

---
+++ customfunctions.py
@@ -0,0 +1,184 @@
+import argparse
+from datetime import datetime
+import datetime
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from db import insertQueueLength
+
+
+
+
+
+def peopleCounter(det, image):
+    print("Detections")
+    print(det)
+    cv2.putText(image, "Person Count : " + str(len(det)) , (0,100), cv2.FONT_HERSHEY_SIMPLEX, fontScale= 1, color=(0, 255, 255), thickness=3)
+
+def peopleCounterReturn(det, image):
+    print("Detections")
+    print(det)
+    cv2.putText(image, "Total People Frame: " + str(len(det)) , (0,100), cv2.FONT_HERSHEY_SIMPLEX, fontScale= 1, color=(0, 255, 255), thickness=3)
+    peopleCount = len(det)
+    return peopleCount
+    
+
+def peopleCounterQueue(indexes_to_keep, image):
+    cv2.putText(image, "old one : " + str(len(indexes_to_keep)) , (0,220), cv2.FONT_HERSHEY_SIMPLEX, fontScale= 1, color=(0, 255, 255), thickness=3)
+    
+
+
+def peopleCounterQueueReturn(indexes_to_keep, image, peopleCount,videoID):
+    print("Detections")
+    print(indexes_to_keep)
+    cv2.putText(image, "QPPL : " + str(peopleCount- len(indexes_to_keep)) , (0,160), cv2.FONT_HERSHEY_SIMPLEX, fontScale= 1, color=(0, 255, 255), thickness=3)
+    cv2.putText(image, "People In the Queue : " + str(len(indexes_to_keep)) , (0,140), cv2.FONT_HERSHEY_SIMPLEX, fontScale= 1, color=(0, 255, 255), thickness=3)
+    # peopleInsideQueue = peopleCount - len(indexes_to_keep)
+    # timeNow = datetime.datetime.now()
+    # insertQueueLength(timeNow,peopleInsideQueue,videoID)
+
+
+    
+
+    
+def calcAvgWaitingTime(image,totalWaitingTime, QueueCount):
+    avg_waiting = totalWaitingTime / QueueCount
+    cv2.putText(image, "Avg Waitig Time: " + str(avg_waiting) , (0,180), cv2.FONT_HERSHEY_SIMPLEX, fontScale= 1, color=(0, 255, 255), thickness=3)
+
+
+
+
+
+def showPredictionsDetails(pred):
+    print("Print Predictions")
+    print(pred)
+    
+    for i in range(len(pred)):
+        print(i)
+
+
+
+
+
+## polygon ROI checking
+# Define Infinite (Using INT_MAX 
+# caused overflow problems)
+INT_MAX = 10000
+# INT_MAX = 100000
+ 
+# Given three colinear points p, q, r, 
+# the function checks if point q lies
+# on line segment 'pr'
+def onSegment(p:tuple, q:tuple, r:tuple) -> bool:
+     
+    if ((q[0] <= max(p[0], r[0])) &
+        (q[0] >= min(p[0], r[0])) &
+        (q[1] <= max(p[1], r[1])) &
+        (q[1] >= min(p[1], r[1]))):
+        return True
+         
+    return False
+ 
+# To find orientation of ordered triplet (p, q, r).
+# The function returns following values
+# 0 --> p, q and r are colinear
+# 1 --> Clockwise
+# 2 --> Counterclockwise
+def orientation(p:tuple, q:tuple, r:tuple) -> int:
+     
+    val = (((q[1] - p[1]) *
+            (r[0] - q[0])) -
+           ((q[0] - p[0]) *
+            (r[1] - q[1])))
+            
+    if val == 0:
+        return 0
+    if val > 0:
+        return 1 # Collinear
+    else:
+        return 2 # Clock or counterclock
+ 
+def doIntersect(p1, q1, p2, q2):
+     
+    # Find the four orientations needed for 
+    # general and special cases
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+ 
+    # General case
+    if (o1 != o2) and (o3 != o4):
+        return True
+     
+    # Special Cases
+    # p1, q1 and p2 are colinear and
+    # p2 lies on segment p1q1
+    if (o1 == 0) and (onSegment(p1, p2, q1)):
+        return True
+ 
+    # p1, q1 and p2 are colinear and
+    # q2 lies on segment p1q1
+    if (o2 == 0) and (onSegment(p1, q2, q1)):
+        return True
+ 
+    # p2, q2 and p1 are colinear and
+    # p1 lies on segment p2q2
+    if (o3 == 0) and (onSegment(p2, p1, q2)):
+        return True
+ 
+    # p2, q2 and q1 are colinear and
+    # q1 lies on segment p2q2
+    if (o4 == 0) and (onSegment(p2, q1, q2)):
+        return True
+ 
+    return False
+ 
+# Returns true if the point p lies 
+# inside the polygon[] with n vertices
+def is_inside_polygon(points:list, p:tuple) -> bool:
+     
+    n = len(points)
+     
+    # There must be at least 3 vertices
+    # in polygon
+    if n < 3:
+        return False
+         
+    # Create a point for line segment
+    # from p to infinite
+    extreme = (INT_MAX, p[1])
+    count = i = 0
+     
+    while True:
+        next = (i + 1) % n
+         
+        # Check if the line segment from 'p' to 
+        # 'extreme' intersects with the line 
+        # segment from 'polygon[i]' to 'polygon[next]'
+        if (doIntersect(points[i],
+                        points[next],
+                        p, extreme)):
+                             
+            # If the point 'p' is colinear with line 
+            # segment 'i-next', then check if it lies 
+            # on segment. If it lies, return true, otherwise false
+            if orientation(points[i], p,
+                           points[next]) == 0:
+                return onSegment(points[i], p,
+                                 points[next])
+                                  
+            count += 1
+             
+        i = next
+         
+        if (i == 0):
+            break
+         
+    # Return true if count is odd, false otherwise
+    return (count % 2 == 1)
